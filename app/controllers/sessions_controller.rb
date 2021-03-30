1b7b3d4de41cc4b69b660805f34ca8c3cdf233ee
rayalva407 <rayalva407@gmail.com>
2021-03-29 23:49:06 -0600

    creates login and signup for users

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = 'Email or password is invalid'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    redirect_to root_path
+  end
+end
